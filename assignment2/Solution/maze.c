4707bb6549fa7f6572b0b7c53cbad427adc7169c
vivekg7 <vivekkumargupta680@gmail.com>
2019-06-09 21:59:36 +0530

    Solution

--- assignment2/Solution/maze.c
+++ assignment2/Solution/maze.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-
+    int n, m, k;
+    scanf("%d %d %d", &n, &m, &k);
+    char maze[n][m], c;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            c = getchar();
+            if (c == '.' || c == '#')
+                maze[i][j] = c;
+            else 
+                j--;
+        }
+    }
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            printf("%c ", maze[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
